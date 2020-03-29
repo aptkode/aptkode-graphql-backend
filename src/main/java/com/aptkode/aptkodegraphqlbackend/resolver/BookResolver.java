b6738de5743cf8e010d3897614362709a3a5e50c
Ruwanka <ruwankamadhushan@gmail.com>
2020-03-29 18:01:48 +0530

    add books query with cursor based pagination

--- src/main/java/com/aptkode/aptkodegraphqlbackend/resolver/BookResolver.java
+++ src/main/java/com/aptkode/aptkodegraphqlbackend/resolver/BookResolver.java
@@ -3,8 +3,13 @@ package com.aptkode.aptkodegraphqlbackend.resolver;
 import com.aptkode.aptkodegraphqlbackend.model.Book;
 import com.aptkode.aptkodegraphqlbackend.repository.BookRepository;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import graphql.relay.Connection;
+import graphql.relay.SimpleListConnection;
+import graphql.schema.DataFetchingEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class BookResolver implements GraphQLQueryResolver {
@@ -14,6 +19,11 @@ public class BookResolver implements GraphQLQueryResolver {
     @Autowired
     public BookResolver(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public Connection<Book> books(int first, String after, DataFetchingEnvironment env) {
+        List<Book> books = bookRepository.findAll();
+        return new SimpleListConnection<>(books).get(env);
     }
 
     public Book getBook(String isbn) {
